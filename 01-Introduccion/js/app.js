e89c03212d0c1219c8800772d354850951dd0a3e
wilder rincon <wilderincon@gmail.com>
2024-01-15 22:13:21 -0500

    JavaScript File

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,19 @@
+//alert("Hola mundo")
+/**const nombre = prompt("cual es tu nombre")
+document.querySelector('.contenido').innerHTML = /*Template  strins*`${nombre} Esta aprendiendo JavaScript Moderno`;+*/
+
+console.log('log');
+console.warn('warn');
+console.error('error');
+console.info('info');
+console.table('table');
+console.time('Hola');
+console.warn('warn');
+console.warn('warn');
+console.warn('warn');
+console.warn('warn');
+console.warn('warn');
+console.warn('warn');
+console.warn('warn');
+console.time('Hola');
+console.loh({nombre: juan})
